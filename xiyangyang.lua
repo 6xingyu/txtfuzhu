a79c7b3a7e4dbfe02ee033a1a544b070035a49e2
6xingyu <118302684+6xingyu@users.noreply.github.com>
2022-11-23 23:21:53 +0800

    Update xiyangyang.lua

--- xiyangyang.lua
+++ xiyangyang.lua
@@ -5,7 +5,7 @@ XR3=Sukor:match("host\":(.-),\"")
 gg.alert("全网启动次数:"..XR.."次\nhost"..XR3.."\nip"..XR2.."")
 
 
-alert("修然")
+alert("KG修然已吧辅助破解无卡密😄")
 
 --function XFYJ(yun)
 ffvpn= "https://vpn.uibe.edu.cn/por/phone_index.csp?rnd=0.23178949332658605#https%3A%2F%2Fvpn.uibe.edu.cn%2F"
