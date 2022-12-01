88669b50f0b74094a0d4942ae07e4d4644c0dbab
6xingyu <118302684+6xingyu@users.noreply.github.com>
2022-12-01 15:14:50 +0800

    Update xiyangyang.lua

--- xiyangyang.lua
+++ xiyangyang.lua
@@ -18,7 +18,6 @@ return
 end end
 if yun==nil then else
 pcall(load(gg.makeRequest(yun).content)) end
-gg.playMusic("https://fanyi.baidu.com/gettts?lan=zh&text=".."欢迎你的到来".."&spd=5&source=wise")
 
 
 local xiuran='https://kgmeimu.zn27.cn/ks.lua'
